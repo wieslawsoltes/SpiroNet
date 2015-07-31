07f3308658a7c2748f8a2b2f6a96c98f7a03c87f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-07-31 23:37:45 +0200

    Removed xml comment

--- Wpf/App.xaml.cs
+++ Wpf/App.xaml.cs
@@ -8,9 +8,6 @@ using System.Windows;
 
 namespace SpiroNet.Wpf
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
