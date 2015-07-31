f64649b77dbb5fb6ebd7e5aff3b4f7e773193ce6
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-07-31 19:25:07 +0200

    Created empty project

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SpiroNet")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SpiroNet")]
+[assembly: AssemblyCopyright("Copyright © Wiesław Šoltés 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("6f958fc2-ebc4-4ddb-9550-1742db599c3d")]
+
+[assembly: AssemblyVersion("0.5.*")]
