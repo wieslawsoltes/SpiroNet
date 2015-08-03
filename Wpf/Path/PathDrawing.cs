43ff74f4b160e9d6de80443324050e7cf25511b0
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-08-03 21:17:25 +0200

    Moved path classes and made them observable

---
+++ Wpf/Path/PathDrawing.cs
@@ -0,0 +1,63 @@
+﻿/*
+SpiroNet.Wpf
+Copyright (C) 2015 Wiesław Šoltés
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301, USA.
+
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpiroNet.Wpf
+{
+    /// <summary>
+    /// The spiro drawing using path shapes geometries. 
+    /// </summary>
+    public class PathDrawing : ObservableObject
+    {
+        private double _width;
+        private double _height;
+        private IList<PathShape> _shapes;
+
+        /// <summary>
+        /// Width of the drawing.
+        /// </summary>
+        public double Width
+        {
+            get { return _width; }
+            set { Update(ref _width, value); }
+        }
+
+        /// <summary>
+        /// Height of the drawing.
+        /// </summary>
+        public double Height
+        {
+            get { return _height; }
+            set { Update(ref _height, value); }
+        }
+
+        /// <summary>
+        /// Path shapes array.
+        /// </summary>
+        public IList<PathShape> Shapes
+        {
+            get { return _shapes; }
+            set { Update(ref _shapes, value); }
+        }
+    }
+}
---
+++ Wpf/Path/PathShape.cs
@@ -0,0 +1,98 @@
+﻿/*
+SpiroNet.Wpf
+Copyright (C) 2015 Wiesław Šoltés
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301, USA.
+
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpiroNet.Wpf
+{
+    /// <summary>
+    /// The spiro shape used to generate Path data.
+    /// </summary>
+    public class PathShape : ObservableObject
+    {
+        private bool _isClosed;
+        private bool _isTagged;
+        private IList<SpiroControlPoint> _points;
+
+        /// <summary>
+        /// Is closed spiro shape.
+        /// Whether points describe a closed (True) or open (False) contour.
+        /// </summary>
+        public bool IsClosed
+        {
+            get { return _isClosed; }
+            set { Update(ref _isClosed, value); }
+        }
+
+        /// <summary>
+        /// Is tagged spiro shape.
+        /// This requires that spiro control points be tagged according to convention. A closed curve will have an extra control point attached to the end of it with a type of 'End'.
+        /// The location of this last point is irrelevant.
+        /// In an open contour the point types of the first and last control points are going to be ignored.
+        /// </summary>
+        public bool IsTagged
+        {
+            get { return _isTagged; }
+            set { Update(ref _isTagged, value); }
+        }
+
+        /// <summary>
+        /// Spiro control points array.
+        /// </summary>
+        public IList<SpiroControlPoint> Points
+        {
+            get { return _points; }
+            set { Update(ref _points, value); }
+        }
+
+        /// <summary>
+        /// Try to generate Path shape data from Points using path bezier context implementation.
+        /// </summary>
+        /// <returns>True when output data was generated successfully.</returns>
+        public bool TryGetData(out string data)
+        {
+            var points = this.Points.ToArray();
+            var bc = new PathBezierContext();
+
+            if (this.IsTagged)
+            {
+                var success = Spiro.TaggedSpiroCPsToBezier0(points, bc);
+                if (success)
+                    data = bc.ToString();
+                else
+                    data = null;
+
+                return success;
+            }
+            else
+            {
+                var success = Spiro.SpiroCPsToBezier0(points, points.Length, this.IsClosed, bc);
+                if (success)
+                    data = bc.ToString();
+                else
+                    data = null;
+
+                return success;
+            }
+        }
+    }
+}
