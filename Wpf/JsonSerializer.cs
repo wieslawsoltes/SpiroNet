9e3f841c4d782b72a6b580097119f26c640c487f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-08-03 16:42:49 +0200

    Added JsonSerializer

---
+++ Wpf/JsonSerializer.cs
@@ -0,0 +1,53 @@
+﻿/*
+SpiroNet.Wpf
+Copyright (C) 2015 Wiesław Šoltés
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301, USA.
+
+*/
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace SpiroNet.Wpf
+{
+    internal static class JsonSerializer
+    {
+        public static string Serialize<T>(T obj) where T : class
+        {
+            var settings = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+                TypeNameHandling = TypeNameHandling.Objects,
+                PreserveReferencesHandling = PreserveReferencesHandling.Objects,
+                ReferenceLoopHandling = ReferenceLoopHandling.Serialize
+            };
+            return JsonConvert.SerializeObject(obj, settings);
+        }
+
+        public static T Deserialize<T>(string json) where T : class
+        {
+            var settings = new JsonSerializerSettings()
+            {
+                TypeNameHandling = TypeNameHandling.Objects,
+                PreserveReferencesHandling = PreserveReferencesHandling.Objects,
+                ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+                ContractResolver = new ObservableCollectionContractResolver()
+            };
+            return JsonConvert.DeserializeObject<T>(json, settings);
+        }
+    }
+}
