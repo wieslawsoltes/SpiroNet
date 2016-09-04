6c46341d95185fa86f8c2711627187dcb5534313
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2016-09-04 18:00:53 +0200

    Moved Main to App class

--- samples/SpiroNet.Avalonia/App.xaml.cs
+++ samples/SpiroNet.Avalonia/App.xaml.cs
@@ -19,15 +19,57 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 
 */
 using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Diagnostics;
+using Avalonia.Logging.Serilog;
 using Avalonia.Markup.Xaml;
+using Serilog;
 
 namespace SpiroNet.Avalonia
 {
     public class App : Application
     {
+        /// <inheritdoc/>
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// Program entry point.
+        /// </summary>
+        /// <param name="args">The program arguments.</param>
+        static void Main(string[] args)
+        {
+            InitializeLogging();
+            AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .Start<MainWindow>();
+        }
+
+        /// <summary>
+        /// Attaches development tools to window in debug mode.
+        /// </summary>
+        /// <param name="window">The window to attach development tools.</param>
+        public static void AttachDevTools(Window window)
+        {
+#if DEBUG
+            DevTools.Attach(window);
+#endif
+        }
+
+        /// <summary>
+        /// Initializes logging in debug mode.
+        /// </summary>
+        private static void InitializeLogging()
+        {
+#if DEBUG
+            SerilogLogger.Initialize(new LoggerConfiguration()
+                .MinimumLevel.Warning()
+                .WriteTo.Trace(outputTemplate: "{Area}: {Message}")
+                .CreateLogger());
+#endif
         }
     }
 }
